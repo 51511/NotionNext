de913e6fd967705e01f272669c4ae51e87d484ce
51511 <109656494+51511@users.noreply.github.com>
2024-03-26 21:18:26 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -21,7 +21,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Leaf', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '我喜歡有關電腦知識', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.leaftechblog.cloudns.biz', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://leaftechblog.cloudns.biz/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Leaftechblog', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
