5478b0b45c11bb3cec4a9967985b9850a7148d10
51511 <109656494+51511@users.noreply.github.com>
2024-10-08 08:06:23 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,12 +1,2 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
-<script type="text/javascript">
-    atOptions = {
-        'key' : '34cc9cdc1485e1ef477da647218111aa',
-        'format' : 'iframe',
-        'height' : 250,
-        'width' : 300,
-        'params' : {}
-    };
-</script>
-<script type="text/javascript" src="//www.topcreativeformat.com/34cc9cdc1485e1ef477da647218111aa/invoke.js"></script>
 
